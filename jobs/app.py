9520b664f28d9cba6d05b25c5de076de69595e1d
LTY4 <laurence@lty4.com>
2020-04-09 18:42:17 -0500

    First try

---
+++ jobs/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+def jobs():
+    return render_template('index.html')
+
+
+def route(url):
+    jobs()
+    return '/'
