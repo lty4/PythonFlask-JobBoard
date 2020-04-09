ca36bf5eacf75caef6804e24b4df53d3e6496297
LTY4 <laurence@lty4.com>
2020-04-09 18:46:17 -0500

    First try

--- jobs/app.py
+++ jobs/app.py
@@ -4,10 +4,8 @@ from flask import render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
 
-
-def route(url):
-    jobs()
-    return '/'
