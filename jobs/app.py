2a53b696ea58e6b5a11d493632849f9cfb2bd2cb
LTY4 <laurence@lty4.com>
2020-04-10 18:57:55 -0500

    Start w/Jinja

--- jobs/app.py
+++ jobs/app.py
@@ -6,10 +6,10 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 
-# def open_connection():
-#     connection = getattr(g, '_connection', None)
-#     if connection is None:
-#         sqlite3.connect(PATH) = connection + g._connection
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    # if connection is None:
+    #     sqlite3.connect(PATH) = connection + g.connection
 
 @app.route('/')
 @app.route('/jobs')
